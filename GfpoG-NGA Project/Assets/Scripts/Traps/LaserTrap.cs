2fb76349d8f371fa2e0f3fc98ebd14d99af8c6c7
Mo0dy <felix.muehlenberend@gmail.com>
2019-01-28 15:12:13 +0100

    added particle effects to lasers

--- GfpoG-NGA Project/Assets/Scripts/Traps/LaserTrap.cs
+++ GfpoG-NGA Project/Assets/Scripts/Traps/LaserTrap.cs
@@ -8,16 +8,21 @@ public class LaserTrap : MonoBehaviour
     [Range(0, 5000)] [SerializeField] private int m_ShootTimeMS = 2000;  // the speed of the projectile
     [Range(0, 10000)] [SerializeField] private int m_ShootStarTOffsetMS = 1000;  // the speed of the projectile
     [SerializeField] private LineRenderer m_LineRenderer;
+    [SerializeField] private ParticleSystem[] m_ImpactSystems;
+    [SerializeField] private ParticleSystem m_LaserParticles;
 
 
     private float m_NextTime;   // the time for the next change
     private bool m_Shooting = false;
     private LayerMask m_Ignore;
+    private float m_OrigEmmisionRate;    // the set emmision rate;
+    private Vector2 m_LastHitPos = Vector2.positiveInfinity;   // stored to save the last hit position so that there is no need to update if nothing changes (+infinity if nothing is hit)
 
     private void Start()
     {
         m_NextTime = Time.timeSinceLevelLoad + m_ShootStarTOffsetMS / 1000;
-    }
+        m_OrigEmmisionRate = m_LaserParticles.emission.rateOverTime.constant;
+    }   
 
     private void FixedUpdate()
     {
@@ -44,7 +49,7 @@ public class LaserTrap : MonoBehaviour
 
     private void Shoot()
     {
-        Vector2 fire_pos = transform.position; // - transform.up;
+        Vector2 firePos = transform.position; // - transform.up;
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position - transform.up, -transform.up, 1000f);//, ~m_Ignore);
 
@@ -55,21 +60,70 @@ public class LaserTrap : MonoBehaviour
             if (player != null)
                 player.Kill();
 
-            m_LineRenderer.SetPosition(0, fire_pos);
-            m_LineRenderer.SetPosition(1, hitInfo.point);
+            
+            OnHIt(firePos, hitInfo.point, hitInfo.normal);
         } else
         {
-            m_LineRenderer.SetPosition(0, fire_pos);
-            m_LineRenderer.SetPosition(1, fire_pos + (Vector2)transform.up * -100);
+            OnMiss(firePos);
         }
     }
-    
-    void OnShootStart()
+
+    private void OnHIt(Vector2 firePos, Vector2 hitPos, Vector2 hitNormal)
     {
-        m_LineRenderer.enabled = true;
+        if (m_LastHitPos != hitPos)
+        {
+            m_LineRenderer.SetPosition(0, firePos);
+            m_LineRenderer.SetPosition(1, hitPos);
+
+            float distance = (firePos - hitPos).magnitude;
+
+            // scale particle emitter
+            var sh = m_LaserParticles.shape;    // get reference to shape struct
+            sh.scale = new Vector3(distance / 2, 1f, 1f);
+            sh.position = new Vector2(0f, -distance / 2);
+
+            // start emitting particles and set impact position
+            foreach (ParticleSystem impactSystem in m_ImpactSystems)
+            {
+                impactSystem.Play();
+                // move to position and rotate to surface normal
+                impactSystem.transform.SetPositionAndRotation(hitPos, Quaternion.LookRotation(Vector3.back, hitNormal));
+            }
+
+            var emission = m_LaserParticles.emission;
+            emission.rateOverTime = distance * m_OrigEmmisionRate;
+        }
+        m_LastHitPos = hitPos;
     }
 
-    void OnShootEnd() {
+    private void OnMiss(Vector2 firePos)
+    {
+        if (m_LastHitPos != Vector2.positiveInfinity)
+        {
+            m_LineRenderer.SetPosition(0, firePos);
+            m_LineRenderer.SetPosition(1, firePos + (Vector2)transform.up * -100);
+
+            var emission = m_LaserParticles.emission;
+            emission.rateOverTime = 100f * m_OrigEmmisionRate;
+
+            // stop impact systems from emitting particles
+            foreach (ParticleSystem impactSystem in m_ImpactSystems)
+            {
+                impactSystem.Stop();
+            }
+        }
+        m_LastHitPos = Vector2.positiveInfinity;
+    }
+
+    private void OnShootStart()
+    {
+        m_LineRenderer.enabled = true;
+        m_LaserParticles.Play();
+    }
+
+    private void OnShootEnd() {
         m_LineRenderer.enabled = false;
+        m_LaserParticles.Stop();
+        OnMiss(Vector2.zero);
     }
 }
